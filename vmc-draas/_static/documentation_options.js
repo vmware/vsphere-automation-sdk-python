c38d418c03e838b9218502a9ea421586df1f7d96
Anusha Hegde <hegdeanusha25@gmail.com>
2019-09-03 12:54:29 +0530

    updated draas docs

--- vmc-draas/_static/documentation_options.js
+++ vmc-draas/_static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.0',
-    LANGUAGE: 'en',
+    VERSION: '6.6.1',
+    LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
