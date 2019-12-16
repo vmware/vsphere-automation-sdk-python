53c72c04c2050e032ba169dd9fe0a5b86bcb6284
amanp <amanp@vmware.com>
2019-12-16 05:26:31 +0000

    Updating latest python vmc doc (318c680)

--- vmc/_static/documentation_options.js
+++ vmc/_static/documentation_options.js
@@ -1,8 +1,9 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.19.0',
+    VERSION: '1.20.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
