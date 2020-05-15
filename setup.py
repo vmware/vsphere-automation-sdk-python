a58810c86e0b3c9bac2a77eac9a5a49f4d35681c
sumitAgrawal007 <asumit@vmware.com>
2020-05-15 20:54:01 +0000

    Updating latest python vmc bindings (ed06abd)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 from setuptools import setup
 
 setup(name='vSphere Automation SDK',
-      version='1.29.0',
+      version='1.30.0',
       description='VMware vSphere Automation SDK for Python',
       url='https://github.com/vmware/vsphere-automation-sdk-python',
       author='VMware, Inc.',
@@ -18,7 +18,7 @@ setup(name='vSphere Automation SDK',
         'vapi-runtime @ file://localhost/{}/lib/vapi-runtime/vapi_runtime-2.15.0-py2.py3-none-any.whl'.format(os.getcwd()),
         'vapi-client-bindings @ file://localhost/{}/lib/vapi-client-bindings/vapi_client_bindings-3.3.0-py2.py3-none-any.whl'.format(os.getcwd()),
         'vapi-common-client @ file://localhost/{}/lib/vapi-common-client/vapi_common_client-2.15.0-py2.py3-none-any.whl'.format(os.getcwd()),
-        'vmc-client-bindings @ file://localhost/{}/lib/vmc-client-bindings/vmc_client_bindings-1.24.0-py2.py3-none-any.whl'.format(os.getcwd()),
+        'vmc-client-bindings @ file://localhost/{}/lib/vmc-client-bindings/vmc_client_bindings-1.25.0-py2.py3-none-any.whl'.format(os.getcwd()),
         'nsx-python-sdk @ file://localhost/{}/lib/nsx-python-sdk/nsx_python_sdk-2.5.1.0.1.15419398-py2.py3-none-any.whl'.format(os.getcwd()),
         'nsx-policy-python-sdk @ file://localhost/{}/lib/nsx-policy-python-sdk/nsx_policy_python_sdk-2.5.1.0.1.15419398-py2.py3-none-any.whl'.format(os.getcwd()),
         'nsx-vmc-policy-python-sdk @ file://localhost/{}/lib/nsx-vmc-policy-python-sdk/nsx_vmc_policy_python_sdk-2.5.1.0.1.15419398-py2.py3-none-any.whl'.format(os.getcwd()),
