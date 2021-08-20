244dac7293ba9338624f96562e7187e90abd0132
sumitAgrawal007 <asumit@vmware.com>
2021-08-20 04:26:22 +0000

    Updating latest python vmc doc (ef1260f)

--- vmc/_static/documentation_options.js
+++ vmc/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.49.0',
+    VERSION: '1.50.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
