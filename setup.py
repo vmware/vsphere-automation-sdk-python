6e7906a036eb27b764d51d2456a39c00702e0564
burenk0v <63559724+burenk0v@users.noreply.github.com>
2022-01-19 22:25:23 +0300

    add comments
    add comments
    
    Signed-off-by: Oleg Burenkov <burenkov.oleg@gmail.com>

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
 import os
+
 from pip.req import parse_requirements
 from setuptools import setup
 
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
 install_reqs = parse_requirements(<requirements_path>)
+
+# reqs is a list of requirement
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
 reqs = [str(ir.req) for ir in install_reqs]
 
 setup(name='vSphere Automation SDK',
