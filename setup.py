b402d39c69baa3bf6166b329bfc7da54e16eccbf
burenk0v <63559724+burenk0v@users.noreply.github.com>
2022-01-19 22:42:52 +0300

    Update setup.py
    In the old version, I can't use my local pypi server to store the package. Using the requirements.txt file as source for install_requires solves this problem.
    
    Signed-off-by: Oleg Burenkov <burenkov.oleg@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,15 @@
 
 import os
 
+from pip.req import parse_requirements
 from setuptools import setup
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements(<requirements_path>)
+
+# reqs is a list of requirement
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(name='vSphere Automation SDK',
       version='1.71.0',
@@ -10,17 +18,5 @@ setup(name='vSphere Automation SDK',
       url='https://github.com/vmware/vsphere-automation-sdk-python',
       author='VMware, Inc.',
       license='MIT',
-      install_requires=[
-        'lxml >= 4.3.0',
-        'pyVmomi >= 6.7',
-        'vapi-runtime @ file://localhost/{}/lib/vapi-runtime/vapi_runtime-2.30.0-py2.py3-none-any.whl'.format(os.getcwd()),
-	      'vapi-client-bindings @ file://localhost/{}/lib/vapi-client-bindings/vapi_client_bindings-3.7.0-py2.py3-none-any.whl'.format(os.getcwd()),
-	      'vapi-common-client @ file://localhost/{}/lib/vapi-common-client/vapi_common_client-2.30.0-py2.py3-none-any.whl'.format(os.getcwd()),
-        'vmc-client-bindings @ file://localhost/{}/lib/vmc-client-bindings/vmc_client_bindings-1.54.0-py2.py3-none-any.whl'.format(os.getcwd()),
-        'nsx-python-sdk @ file://localhost/{}/lib/nsx-python-sdk/nsx_python_sdk-3.1.3.2.0-py2.py3-none-any.whl'.format(os.getcwd()),
-        'nsx-policy-python-sdk @ file://localhost/{}/lib/nsx-policy-python-sdk/nsx_policy_python_sdk-3.1.3.2.0-py2.py3-none-any.whl'.format(os.getcwd()),
-        'nsx-vmc-policy-python-sdk @ file://localhost/{}/lib/nsx-vmc-policy-python-sdk/nsx_vmc_policy_python_sdk-3.1.3.2.0-py2.py3-none-any.whl'.format(os.getcwd()),
-        'nsx-vmc-aws-integration-python-sdk @ file://localhost/{}/lib/nsx-vmc-aws-integration-python-sdk/nsx_vmc_aws_integration_python_sdk-3.1.3.2.0-py2.py3-none-any.whl'.format(os.getcwd()),
-        'vmc-draas-client-bindings @ file://localhost/{}/lib/vmc-draas-client-bindings/vmc_draas_client_bindings-1.18.0-py2.py3-none-any.whl'.format(os.getcwd()),
-      ]
+      install_requires=reqs
 )
