f32e6cfde32cdad2dc1f6cdb6ccef9018972f7df
shweta purohit <spurohit@vmware.com>
2022-07-14 16:36:32 +0530

    vmc19 and vc-8.0.0.0 respin

--- nsx/nsx_vmc_aws_integration/_static/documentation_options.js
+++ nsx/nsx_vmc_aws_integration/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.1.2.1.1',
+    VERSION: '4.0.0.0.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
