4d71ecdce96612e6fa55f23b79695f36f8a48738
Tohar <73241510+Tohar-orca@users.noreply.github.com>
2023-01-12 17:35:15 +0200

    Align package naming convention
    https://peps.python.org/pep-0008/#package-and-module-names
    
    Signed-off-by: Tohar Braun <tohar@orca.security>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 from setuptools import setup
 
-setup(name='vSphere Automation SDK',
+setup(name='vsphere-automation-sdk-python',
       version='1.80.0',
       description='VMware vSphere Automation SDK for Python',
       url='https://github.com/vmware/vsphere-automation-sdk-python',
