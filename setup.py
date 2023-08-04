9ade0da965144ac0e50cbbfa94db8a338f529a87
Ankit Agrawal <aagrawal3@vmware.com>
2023-08-04 11:10:37 +0530

    Rename vmc-bindings to vmwarecloud-aws and vmc-draas-bindings to vmwarecloud-draas. Signed-off-by: Ankit Agrawal <aagrawal3@vmware.com>

--- setup.py
+++ setup.py
@@ -17,11 +17,11 @@ setup(name='vsphere-automation-sdk',
         'vapi-runtime @ file://localhost/{}/lib/vapi-runtime/vapi_runtime-2.40.0-py2.py3-none-any.whl'.format(os.getcwd()),
         'vcenter-bindings @ file://localhost/{}/lib/vcenter-bindings/vcenter_bindings-4.1.0-py2.py3-none-any.whl'.format(os.getcwd()),
         'vapi-common-client @ file://localhost/{}/lib/vapi-common-client/vapi_common_client-2.40.0-py2.py3-none-any.whl'.format(os.getcwd()),
-        'vmc-bindings @ file://localhost/{}/lib/vmc-bindings/vmc_bindings-1.62.0-py2.py3-none-any.whl'.format(os.getcwd()),
+        'vmwarecloud-aws @ file://localhost/{}/lib/vmwarecloud-aws/vmwarecloud_aws-1.62.0-py2.py3-none-any.whl'.format(os.getcwd()),
         'nsx-python-sdk @ file://localhost/{}/lib/nsx-python-sdk/nsx_python_sdk-4.1.0.1.0-py2.py3-none-any.whl'.format(os.getcwd()),
         'nsx-policy-python-sdk @ file://localhost/{}/lib/nsx-policy-python-sdk/nsx_policy_python_sdk-4.1.0.1.0-py2.py3-none-any.whl'.format(os.getcwd()),
         'nsx-vmc-policy-python-sdk @ file://localhost/{}/lib/nsx-vmc-policy-python-sdk/nsx_vmc_policy_python_sdk-4.1.0.1.0-py2.py3-none-any.whl'.format(os.getcwd()),
         'nsx-vmc-aws-integration-python-sdk @ file://localhost/{}/lib/nsx-vmc-aws-integration-python-sdk/nsx_vmc_aws_integration_python_sdk-4.1.0.1.0-py2.py3-none-any.whl'.format(os.getcwd()),
-        'vmc-draas-bindings @ file://localhost/{}/lib/vmc-draas-bindings/vmc_draas_bindings-1.21.0-py2.py3-none-any.whl'.format(os.getcwd()),
+        'vmwarecloud-draas @ file://localhost/{}/lib/vmwarecloud-draas/vmwarecloud_draas-1.21.0-py2.py3-none-any.whl'.format(os.getcwd()),
       ]
 )
