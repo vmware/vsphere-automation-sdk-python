02f49dd2f2b9a49398f5a424835f4b16b5fd67f2
aagrawal3 <aagrawal3@vmware.com>
2023-08-04 09:02:56 +0000

    Updating latest python vmwarecloud-aws bindings (22ba9ff). Signed-off-by: aagrawal3 <aagrawal3@vmware.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 from setuptools import setup
 
 setup(name='vsphere-automation-sdk',
-      version='1.82.0',
+      version='1.83.0',
       description='VMware vSphere Automation SDK for Python',
       url='https://github.com/vmware/vsphere-automation-sdk-python',
       author='VMware, Inc.',
@@ -17,7 +17,7 @@ setup(name='vsphere-automation-sdk',
         'vapi-runtime @ file://localhost/{}/lib/vapi-runtime/vapi_runtime-2.40.0-py2.py3-none-any.whl'.format(os.getcwd()),
         'vcenter-bindings @ file://localhost/{}/lib/vcenter-bindings/vcenter_bindings-4.1.0-py2.py3-none-any.whl'.format(os.getcwd()),
         'vapi-common-client @ file://localhost/{}/lib/vapi-common-client/vapi_common_client-2.40.0-py2.py3-none-any.whl'.format(os.getcwd()),
-        'vmwarecloud-aws @ file://localhost/{}/lib/vmwarecloud-aws/vmwarecloud_aws-1.62.0-py2.py3-none-any.whl'.format(os.getcwd()),
+        'vmwarecloud-aws @ file://localhost/{}/lib/vmwarecloud-aws/vmwarecloud_aws-1.63.0-py2.py3-none-any.whl'.format(os.getcwd()),
         'nsx-python-sdk @ file://localhost/{}/lib/nsx-python-sdk/nsx_python_sdk-4.1.0.1.0-py2.py3-none-any.whl'.format(os.getcwd()),
         'nsx-policy-python-sdk @ file://localhost/{}/lib/nsx-policy-python-sdk/nsx_policy_python_sdk-4.1.0.1.0-py2.py3-none-any.whl'.format(os.getcwd()),
         'nsx-vmc-policy-python-sdk @ file://localhost/{}/lib/nsx-vmc-policy-python-sdk/nsx_vmc_policy_python_sdk-4.1.0.1.0-py2.py3-none-any.whl'.format(os.getcwd()),
